bfa6e8c6cb614638ed610041b985508c87b3cdfe
alekcandr1 <alekcandrmain@gmail.com>
2024-02-15 13:47:08 +0300

    add useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Accordion } from './components/Accordion/accordion';
+import { Rating } from './components/Rating/Rating';
+import { AppTitle } from './components/App-title';
+import OnOff from './components/OnOff';
 
+// function declaration
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log('App rendering');
+    return (
+        <div>
+            <AppTitle />
+
+            {/*<Accordion title={ 'Title menu' }/>*/ }
+            <Rating />
+            {/*<OnOff/>*/ }
+
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // отрисовывает элементы DOM
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -13,7 +13,8 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+// ReactDOM.render(<hr />, document.getElementById('root'));
+
+
 reportWebVitals();
